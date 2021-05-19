c888a061e4ee35657c7d46d341b6a44167813e67
Ilene Seelemann <ilene@ca.ibm.com>
2021-05-19 14:27:40 -0400

    Update main.tf
    Use provider version 1.24

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
+      version = ">= 1.24.0"
     }
   }
 }
@@ -14,7 +15,6 @@ terraform {
 provider "ibm" {
   /* Uncomment ibmcloud_api_key while testing from CLI */
   // ibmcloud_api_key = var.api_key
-  generation       = 2
   region           = var.region
   ibmcloud_timeout = 300
 }
